1b574d597894cae2caf8ec2d0a7c47895d5b368c
Wadah Esam <wadah-wleed@hotmail.com>
2024-10-15 20:14:19 +0300

    chore: better example

--- App.tsx
+++ App.tsx
@@ -1,38 +1,60 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
 
-import React from 'react';
-import {
-  SafeAreaView,
-  StatusBar,
-  useColorScheme,
-  View,
-} from 'react-native';
+import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
+import AnimateableText from 'react-native-animateable-text';
+import { useAnimatedProps, useSharedValue } from 'react-native-reanimated';
+import {Slider} from '@miblanchard/react-native-slider';
 
-import {
-  Colors,
-} from 'react-native/Libraries/NewAppScreen';
+const style = {
+  fontSize: 30,
+  fontWeight: 'bold' as const,
+  color: '#7832C7' as const,
+  paddingTop: 0,
+  paddingBottom: 0,
+};
 
-function App(): React.JSX.Element {
-  const isDarkMode = useColorScheme() === 'dark';
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  row: { flexDirection: 'row', paddingLeft: 20, paddingRight: 20 },
+});
 
-  const backgroundStyle = {
-    backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
-  };
+export default function App() {
+  React.useEffect(() => {
+    // checking if the app is in new arch or not
+    // @ts-ignore
+    const uiManager = global?.nativeFabricUIManager ? 'Fabric' : 'Paper';
+    console.log(`Using ${uiManager}`);
+  }, []);
+
+  const stringSharedValue = useSharedValue('1');
+
+  const animatedProps = useAnimatedProps(() => {
+    return {
+      text: stringSharedValue.value,
+    };
+  });
 
   return (
-    <SafeAreaView style={backgroundStyle}>
-      <StatusBar
-        barStyle={isDarkMode ? 'light-content' : 'dark-content'}
-        backgroundColor={backgroundStyle.backgroundColor}
+    <View style={styles.container}>
+      <Slider
+        containerStyle={{
+          width: '80%',
+        }}
+        minimumValue={0}
+        step={1}
+        maximumValue={10000}
+        minimumTrackTintColor="pink"
+        maximumTrackTintColor="violet"
+        thumbTintColor="#7832C7"
+        onValueChange={(value) => {
+          stringSharedValue.value = `${value}`;
+        }}
       />
-      <View style={{ height: 100, width: 100, backgroundColor: 'red'}} />
-    </SafeAreaView>
+      <AnimateableText selectable style={style} animatedProps={animatedProps} />
+    </View>
   );
 }
-
-export default App;
